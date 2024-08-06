b606f419f2af9aff9c28e83882a24768c5812ec2
MacBook Pro <admin@admin.local>
2024-08-06 17:14:04 +0700

    put method

--- api/handlers/meter_handler.py
+++ api/handlers/meter_handler.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
 from utils.file_utils import read_file
+import json
+import os
 
 def get_meter_data(meter_id: str):
     data = read_file()
@@ -14,3 +16,16 @@ def get_meter_display_name(meter_id: str):
         if meter_data['meter_id'] == meter_id:
             return {"display_name": meter_data['display_name']}
     raise HTTPException(status_code=404, detail="Meter not found")
+
+def update_meter_state(meter_id: str, state: bool):
+    print(f"Updating meter state for {meter_id} to {state}")  # Debugging statement
+    data = read_file()
+    for meter_data in data:
+        if meter_data['meter_id'] == meter_id:
+            meter_data['state'] = state
+            # Assuming you want to save the updated data back to the file
+            file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config', 'default.json')
+            with open(file_path, 'w') as file:
+                json.dump(data, file, indent=4)
+            return {"message": "Meter state updated successfully"}
+    raise HTTPException(state_code=404, detail="Meter not found")
--- api/routes/meter_routes.py
+++ api/routes/meter_routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from handlers.meter_handler import get_meter_data, get_meter_display_name
+from handlers.meter_handler import get_meter_data, get_meter_display_name, update_meter_state
+from schemas.meter_schema import MeterStateUpdate
 
+import json
+from fastapi import Body
 router = APIRouter()
 
 @router.get("/meter/{meter_id}")
@@ -10,3 +13,12 @@ async def get_meter(meter_id: str):
 @router.get("/name/{meter_id}")
 async def get_meter_display_name_route(meter_id: str):
     return get_meter_display_name(meter_id)
+
+# # update meter status
+# @router.put("/meter/{meter_id}")
+# async def update_meter_status(meter_id: str, status: str):
+#     return update_meter_status(meter_id, status)
+
+@router.put("/meter/{meter_id}/state")
+async def update_meter_state_route(meter_id: str, meter_state: MeterStateUpdate):
+    return update_meter_state(meter_id, meter_state.state)
