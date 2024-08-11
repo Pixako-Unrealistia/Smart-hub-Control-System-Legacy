468568628bc1371953773a5bd67444ddf163805f
MacBook Pro <admin@admin.local>
2024-08-11 22:32:42 +0700

    update layout

--- smart-hub-web/src/app/layout.js
+++ smart-hub-web/src/app/layout.js
@@ -12,18 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      </head>
       <body className={inter.className}>
         <AuthProvider>
           {children}
